d5ccf5e16c45421764ae31836ccb817da69e996d
Meet patel <jaymeet2005@gmail.com>
2025-10-01 16:22:14 +0000

    image not loaded

--- next.config.ts
+++ next.config.ts
@@ -23,8 +23,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
       },
